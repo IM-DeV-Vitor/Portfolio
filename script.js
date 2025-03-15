4993459bb8793a2fff271166637fae739fdd5155
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-03-14 22:22:16 -0400

    Rework TOTAL
    Estilização totalmente nova, espero que gostem :D

---
+++ script.js
@@ -0,0 +1,14 @@
+var time = new Date()
+var hours = 19//time.getHours()
+var span = document.getElementById("alter")
+
+if (hours < 12){
+    document.body.style.backgroundColor = "#679CFF"
+    span.innerText = "Olá, eu sou o Vitor! Bom Dia!"
+} else if (hours < 18 && hours > 12){
+    document.body.style.backgroundColor = "#12B0FF"
+    span.innerText = "Olá, eu sou o Vitor! Boa Tarde!"
+} else {
+    document.body.style.backgroundColor = "#12FBFF"
+    span.innerText = "Olá, eu sou o Vitor! Boa Noite!"
+}
