b9f73372905f535d8fbc4f93d69ba6be0f78ef09
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-03-15 18:09:54 -0400

    Ajustezinho

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var hours = time.getHours()
 var span = document.getElementById("alter")
 var header = document.getElementById("saudar")
 var sobre_mim = document.getElementById("sobre-mim")
+var tag_a_link_informativo = document.getElementById("link_informativo")
 
 if (hours < 13){
     document.body.style.backgroundColor = "#B1E602"
@@ -19,4 +20,5 @@ if (hours < 13){
     header.style.backgroundColor = "#5712FF"
     sobre_mim.style.backgroundColor = "#5712FF"
     span.innerHTML = "Olá, eu sou o Vitor!<br>Boa Noite!"
+    tag_a_link_informativo.style.backgroundColor = "white"
 }
