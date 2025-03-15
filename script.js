64d4399941e776aa05d37d41c6c19e528f05d4df
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-03-15 12:42:02 -0400

    Interatividade com Horário

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var time = new Date()
 var hours = time.getHours()
 var span = document.getElementById("alter")
+var header = document.getElementById("saudar")
+var sobre_mim = document.getElementById("sobre-mim")
 
-if (hours < 12){
-    document.body.style.backgroundColor = "#679CFF"
+if (hours < 13){
+    document.body.style.backgroundColor = "#B1E602"
+    header.style.backgroundColor = "#02E60F"
+    sobre_mim.style.backgroundColor = "#02E60F"
     span.innerText = "Olá, eu sou o Vitor! Bom Dia!"
-} else if (hours < 18 && hours > 12){
-    document.body.style.backgroundColor = "#12B0FF"
+} else if (hours >= 13 && hours < 18){
+    document.body.style.backgroundColor = "#E68700"
+    header.style.backgroundColor = "#E61102"
+    sobre_mim.style.backgroundColor = "#E61102"
     span.innerText = "Olá, eu sou o Vitor! Boa Tarde!"
 } else {
-    document.body.style.backgroundColor = "#12FBFF"
+    document.body.style.backgroundColor = "#679CFF"
+    header.style.backgroundColor = "#5712FF"
+    sobre_mim.style.backgroundColor = "#5712FF"
     span.innerText = "Olá, eu sou o Vitor! Boa Noite!"
 }
