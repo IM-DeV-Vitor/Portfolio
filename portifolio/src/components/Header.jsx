f807fb3cdbb95c2a168819fc9e049a903880abc9
Vitor <vitorhugo1235lll@gmail.com>
2025-11-09 18:14:36 -0400

    Done

--- portifolio/src/components/Header.jsx
+++ portifolio/src/components/Header.jsx
@@ -2,12 +2,26 @@ import styles from "../styles/Header.module.css"
 import catImage from "../assets/privateinvestocat.jpg"
 import { FaHome, FaPhoneAlt, FaQuestion, FaPencilAlt } from "react-icons/fa";
 import { PiBagSimpleFill } from "react-icons/pi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useRef, useState } from "react";
 
 export default function Header() {
     const date = new Date
+    const boxRef = useRef(null)
+    const [isOpen, setIsOpen] = useState(false)
     let horas = date.getHours()
     let minutos = date.getMinutes()
-
+    
+    const openBox = () => {
+        if (!isOpen) {
+            setIsOpen(true)
+            boxRef.current.style.display = "flex"
+            boxRef.current.classList.add("animatedClass")
+        } else {
+            setIsOpen(false)
+            boxRef.current.style.display = "none"
+        }
+    }
     return (
         <div className={styles.headerContainer}>
             <div className={styles.imgContainer}>
@@ -22,6 +36,21 @@ export default function Header() {
                 <div className={styles.iconnedLink}><a href="#">Skills</a> <FaPencilAlt /></div>
                 <div className={styles.iconnedLink}><a href="#">Contact</a> <FaPhoneAlt /></div>
             </nav>
+            <div className={styles.menuContainer}>
+                <div className={styles.menuIconBox}>
+                    <GiHamburgerMenu className={styles.menuIcon} style={{display: "block", margin: "auto"}} onClick={openBox} cursor={"pointer"} />
+                </div>
+                <div className={styles.menuItemsBox}>
+                    <nav className={styles.compactNav} ref={boxRef}>
+                        <div className={styles.iconnedLink}><a href="#">Home</a> <FaHome /></div>
+                        <div className={styles.iconnedLink}><a href="#">About</a> <FaQuestion /></div>
+                        <div className={styles.iconnedLink}><a href="#">Projects</a> <PiBagSimpleFill /></div>
+                        <div className={styles.iconnedLink}><a href="#">Skills</a> <FaPencilAlt /></div>
+                        <div className={styles.iconnedLink}><a href="#">Contact</a> <FaPhoneAlt /></div>
+                    </nav>
+                </div>
+            </div>
+
             </div>
 
 
